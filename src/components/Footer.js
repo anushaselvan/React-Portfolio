112d1a9cad43ff81cba2444c5d475d9987336192
anushaselvan <anushaselvan@gmail.com>
2022-01-25 16:22:04 +1100

    Added Skills

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ import '../styles/Footer.css';
 function Footer() {
     return (
       <div className="Footer">
-        <a href="https://github.com/anushaselvan" target="blank"> 
+        <a href="https://github.com/anushaselvan" target="_blank" rel="noreferrer" > 
             <img src ={require("../components/assets/icons8-github-48.png")} alt="gitHub logo"/></a>
 
-        <a href="https://www.linkedin.com/in/anusha-s-89734421b/" target="blank">
+        <a href="https://www.linkedin.com/in/anusha-s-89734421b/" target="_blank" rel="noreferrer" >
             <img src ={require('../components/assets/icons8-linkedin-48.png')} alt="linkedIn logo"/></a>
    
-        <a href="mailto:anushaselvan@gmail.com?" target="blank">
+        <a href="mailto:anushaselvan@gmail.com?" target="_blank" rel="noreferrer" >
             <img src ={require('../components/assets/icons8-mail-48.png')} alt="mail icon"/></a>
 
       </div>
